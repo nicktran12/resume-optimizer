1b2147e487c412a85a82d49ec1d7932986f2e47a
Nicholas Tran <ntran62@ucsc.edu>
2026-09-14 12:10:19 -0700

    fix rate limiting

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ settings = get_settings()
 
 app = FastAPI(title=settings.APP_NAME)
 app.state.limiter = limiter
-app.add_exception_handlder(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
   CORSMiddleware,
